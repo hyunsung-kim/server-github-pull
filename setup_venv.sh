23c189a6123408962ea707895d686b086df4b275
hyunsung_kim <hyunsung.kim@superb-ai.com>
2020-04-18 17:45:07 +0900

    Fix ubuntu 18 version error

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-sudo apt-get install python3-venv
-
-mkdir .venv
-cd .venv
+apt-get update
+sudo apt-get install python3 python3-venv python3-pip
 
 python3 -m venv .venv
 source ./.venv/bin/activate
